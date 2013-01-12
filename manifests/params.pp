22762a5724a30b8e0e5320d0b676d76ee37ebafb
Alessandro Franceschi <al@lab42.it>
2013-01-12 14:01:04 +0100

    Fix for https://github.com/example42/puppet-ntp/issues/12

--- manifests/params.pp
+++ manifests/params.pp
@@ -26,11 +26,12 @@ class ntp::params {
   ### Application related parameters
 
   $package = $::operatingsystem ? {
-    'Solaris' => $::operatingsystemrelease ? {
+    /(?i:Debian|Ubuntu|Mint)/ => [ 'ntp' , 'ntpdate' ],
+    'Solaris'                 => $::operatingsystemrelease ? {
       '5.10'  => [ 'SUNWntpr' , 'SUNWntpu' ],
       default => 'ntp',
     },
-    default   => 'ntp',
+    default                   => 'ntp',
   }
 
   $service = $::operatingsystem ? {
